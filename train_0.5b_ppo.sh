175e89ec5eef29c36696107326abc2b985ca19e7
lizhieffe <lizhieffe@gmail.com>
2026-02-22 20:17:00 -0800

    update script based on my setup

--- train_0.5b_ppo.sh
+++ train_0.5b_ppo.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# alias python='/home/weiji/anaconda3/envs/zero/bin/python'
-# alias python3='/home/weiji/anaconda3/envs/zero/bin/python3'
-# alias pip='/home/weiji/anaconda3/envs/zero/bin/pip'
 
 export N_GPUS=1
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 ray stop --force && ray start --head --include-dashboard=True
 export BASE_MODEL="model/Qwen2.5-0.5B"
 export DATA_DIR="data/countdown"
